23e506d8915e67f81c0c30773eb960e877952b6c
tim-hadman <94804617+tim-hadman@users.noreply.github.com>
2021-12-27 17:11:56 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,143 +1,39 @@
-$(function(){
-$('.slider__inner').slick({
-   infinite:false,
-   nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-   prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-   autoplay:true
-    
-})
+$ (function(){
 
-$('.Testimonialst__items-inner').slick({
-  infinite:true,
-  autoplay:false,
-  nextArrow:false,
-    prevArrow:false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay:true,
-    responsive: [
-     
-        {
-          breakpoint: 1050,
-          settings: {
-            slidesToShow: 2,
-  
-          }
-        },
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            rows:1,
-          }
-        }
-        
-        ]
+    $('.news__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: false,
+    autoplaySpeed: 3000,
+      });
+      $('.clients__items, .partners__items').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        autoplay: true,
+    autoplaySpeed: 3000,
+      });
+      $('.testimonials__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+    autoplaySpeed: 3000,
+      });
+      $('.works__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: false,
+    autoplaySpeed: 3000,
+      });
 })
-$('.news__slider').slick({
-    nextArrow:false,
-    prevArrow:false,
-    dots: true,
-    infinite:false,
-    slidesToShow: 2,
-    responsive: [
-     
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            dots: false,
-            rows:1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            rows:1,
-          }
-        }
-        
-        ]
-})
-$('.partners__slider').slick ({
-  nextArrow:false,
-  prevArrow:false,
-  
-  infinite:true,
-  slidesToShow:6,
-   slidesToScroll: 6,
-  slidesPerRow: 6,
-  autoplay:true,
-  responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          infinite: true,
-          
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      }
-      
-    ]
-})
-$('.clients__slider').slick ({
-  nextArrow:false,
-  prevArrow:false,
-  dots: false,
-  infinite:true,
-  slidesToShow:6,
-  rows:1,
-  slidesToScroll: 6,
-  slidesPerRow: 6,
-  autoplay:true,
-  responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      }
-      
-    ]
-})
-
-$('.header__btn-menu ').on('click', function(){
-$('.menu ul').slideToggle();
-});
-
-});
